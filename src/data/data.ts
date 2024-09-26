60218583cd762cfd1034e6d5682fd36fb70abee7
Rafael <radije3bros@hotmail.com>
2024-09-25 22:51:51 -0600

    Configuración del proyecto y creación del Header

---
+++ src/data/data.ts
@@ -0,0 +1,107 @@
+export const navs = [
+  {
+    id: 1,
+    name: 'Inicio',
+    link: '/',
+    active: true,
+  },
+  {
+    id: 2,
+    name: 'Publicaciones',
+    link: '/postitems',
+    active: false,
+  },
+  {
+    id: 3,
+    name: 'Crear Publicación',
+    link: '/createpostitems',
+    active: false,
+  },
+  {
+    id: 4,
+    name: 'Acerca de',
+    link: '/about',
+    active: false,
+  },
+  {
+    id: 5,
+    name: 'Contacto',
+    link: '/contact',
+    active: false,
+  },
+];
+
+export const scis = [
+  {
+    id: 1,
+    icon: 'bi-facebook',
+    link: 'https://www.facebook.com/',
+  },
+  {
+    id: 2,
+    icon: 'bi-twitter-x',
+    link: 'https://x.com/X',
+  },
+  {
+    id: 3,
+    icon: 'bi-instagram',
+    link: 'https://www.instagram.com/',
+  },
+];
+
+export const heroSlides = [
+  {
+    id: 1,
+    bgImg: 'assets/img/post-slide-1.jpg',
+    title: 'The Best Homemade Masks for Face (keep the Pimples Away)',
+    brief: `Lorem ipsum dolor sit amet, consectetur adipisicing
+    elit. Quidem neque est mollitia! Beatae minima assumenda
+    repellat harum vero, officiis ipsam magnam obcaecati
+    cumque maxime inventore repudiandae quidem
+    necessitatibus rem atque.`,
+  },
+  {
+    id: 2,
+    bgImg: 'assets/img/post-slide-2.jpg',
+    title:
+      '17 Pictures of Medium Length Hair in Layers That Will Inspire Your New Haircut',
+    brief: `Lorem ipsum dolor sit amet, consectetur adipisicing
+    elit. Quidem neque est mollitia! Beatae minima assumenda
+    repellat harum vero, officiis ipsam magnam obcaecati
+    cumque maxime inventore repudiandae quidem
+    necessitatibus rem atque.`,
+  },
+  {
+    id: 3,
+    bgImg: 'assets/img/post-slide-3.jpg',
+    title: '9 Half-up/half-down Hairstyles for Long and Medium Hair',
+    brief: `Lorem ipsum dolor sit amet, consectetur adipisicing
+    elit. Quidem neque est mollitia! Beatae minima assumenda
+    repellat harum vero, officiis ipsam magnam obcaecati
+    cumque maxime inventore repudiandae quidem
+    necessitatibus rem atque.`,
+  },
+  {
+    id: 4,
+    bgImg: 'assets/img/post-slide-4.jpg',
+    title: '13 Amazing Poems from Shel Silverstein with Valuable Life Lessons',
+    brief: `Lorem ipsum dolor sit amet, consectetur adipisicing
+    elit. Quidem neque est mollitia! Beatae minima assumenda
+    repellat harum vero, officiis ipsam magnam obcaecati
+    cumque maxime inventore repudiandae quidem
+    necessitatibus rem atque.`,
+  },
+];
+
+export const contact = [
+  {
+    id: 1,
+    details: {
+      name: 'Karla Adelgreen',
+      email: 'DonOsamar18@uptap.com',
+      subject: 'Message',
+      message: `Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+    velit esse cillum dolore eu fugiat nulla pariatur`,
+    },
+  },
+];
